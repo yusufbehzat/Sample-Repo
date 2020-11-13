e40912fd28e767776413788d293707b36089369d
Yusuf Behzat <yusufbehzat@gmail.com>
2020-11-13 19:21:07 +0300

    Removed Some Code

--- script.py
+++ script.py
@@ -1,20 +1,5 @@
-import math
-import os
-import sys
+import requests
 
-# import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# r = requests.get("https://www.google.com")
-# print(r.status_code)
-
-print(greet("World"))
-print(greet("Yusuf"))
+r = requests.get("https://www.tureng.com")
+print(r.status_code)
+print(r.ok)
